5384c7db3c9143e5b156ac3ff2ab54eee1e81285
DogeSec <utdoge@gmail.com>
2017-10-02 13:30:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+impor os 
 def bot_token(token):
   token = input("Enter your bots token: ") #Working on it :D
+  
+  os.system('python bot.py')
   
 if __name__ == '__main__':
   bot_token():                                    
